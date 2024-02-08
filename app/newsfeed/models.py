cd3b03d40fa6b4ad7d0d5905d28ef66a58173575
Павел <naveq4358@gmail.com>
2024-02-08 19:09:17 +0300

    start new django app- newsfeed

---
+++ app/newsfeed/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+from blog import Post
+
+class NewsFeed(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    read = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('user', 'post')
