65d347dd64f8f1dbe0214faa89683f34cff99aa4
Павел <naveq4358@gmail.com>
2024-02-08 17:16:29 +0300

    Configured PostgreSQL database

---
+++ app/conf/components/database.py
@@ -0,0 +1,12 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PG_DATABASE'),
+        'USER': os.environ.get('PG_USER'),
+        'PASSWORD': os.environ.get('PG_PASSWORD'),
+        'HOST': os.environ.get('PG_HOST', 'db'),
+        'PORT': os.environ.get('PG_PORT'),
+    }
+}
